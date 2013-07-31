3cbb777ab2faf3abaa9eef85913e78b545107f8e
archita <architajain.princy@gmail.com>
2013-07-31 09:57:22 -0700

    rails app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsapp::Application.config.secret_key_base = '41722882403ca335393c713578b0b0ca95558d8b2e241704593c37d6fe1ed5e9c69962f1dbf5ec9fe164896a51aa1ab34a717180988e0bc4fcd8563629211a0a'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Archita"
+    email    "architajain@gmail.com"
+    password "architatest"
+    password_confirmation "architatest"
+  end
+end
